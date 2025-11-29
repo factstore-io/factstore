a67862e97faafb149775fe0b17fd3e4b8276a2fc
Domenic Cassisi <domenic.cassisi@web.de>
2025-11-29 22:23:24 +0100

    dependency upgrade

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-    kotlin("jvm") version "2.2.20"
-    kotlin("plugin.allopen") version "2.2.20"
-    kotlin("plugin.serialization") version "2.2.20"
+    kotlin("jvm") version "2.2.21"
+    kotlin("plugin.allopen") version "2.2.21"
+    kotlin("plugin.serialization") version "2.2.21"
     id("io.quarkus")
 }
 
