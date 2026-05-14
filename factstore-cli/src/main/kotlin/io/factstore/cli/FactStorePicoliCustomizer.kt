5f54694f82597d47c09ef095d9fe57c5896db292
Domenic Cassisi <domenic.cassisi@web.de>
2026-05-14 15:17:19 +0200

    #161 support different output formats

--- factstore-cli/src/main/kotlin/io/factstore/cli/FactStorePicoliCustomizer.kt
+++ factstore-cli/src/main/kotlin/io/factstore/cli/FactStorePicoliCustomizer.kt
@@ -16,5 +16,6 @@ class FactStorePicoliCustomizer(
         factory
             .create()
             .setExecutionExceptionHandler(exceptionHandler)
+            .setCaseInsensitiveEnumValuesAllowed(true)
 
 }
